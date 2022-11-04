074fea527a4b753d82f797e6cc91bff0271bfae7
mahaboob jani <skmabujani521@gmail.com>
2022-11-04 23:14:50 +0530

    mahaboob

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Movie from './components/Movie';
 
 function App() {
+
+ 
+  
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+      <div  className='App'>
+       
+     <Movie img={'https://i.pinimg.com/originals/8a/f3/61/8af36186ee2e3bf86e466b8fc1ca5cc6.jpg'}
+       name={ 'Sarileru'}  year={'2020 , mahesh'} time={'119 min'} about={' Action, Drama'}
+         rating={'https://o.remove.bg/downloads/26a2c8c8-41d5-46b0-a9b0-d1d92d1354b6/7d4cf105cef06615cd1948b7af70a7d4-removebg-preview.png'}
+         num='4.5'
+         description={'A tough army major is deployed to Kurnool on a mission to keep the country safe from external threats. Director: Anil Ravipudi | Stars: Mahesh Babu, Rashmika Mandanna, Vijayshanti, Rajendra Prasad '}
+         
+         fullimage={'https://wallpaperaccess.com/full/4009615.jpg'}/>
+
+         <Movie img={'https://m.media-amazon.com/images/M/MV5BNzQ3YzkyMjEtNGFmYi00Y2QzLTg4NjItODMzNWQ0NzBiYWEzXkEyXkFqcGdeQXVyNjQ1MDcxNzM@._V1_UX140_CR0,0,140,209_AL_.jpg'}
+       name={ ' Maharshi '}  year={'2022 , mahesh'} time={'119 min'} about={' Action, Comedy'}
+         rating={'https://o.remove.bg/downloads/26a2c8c8-41d5-46b0-a9b0-d1d92d1354b6/7d4cf105cef06615cd1948b7af70a7d4-removebg-preview.png'}
+         num='4.5'
+         description={'Rishi, a millionaire businessman, returns to his homeland, where he becomes the champion of poor and downtrodden farmers.Director: Vamshi Paidipally | Stars: Mahesh Babu, Allari Naresh, Pooja Hegde, Nicholas Pinto '}
+         
+         fullimage={'https://i0.wp.com/www.socialnews.xyz/wp-content/uploads/2019/05/06/Mahesh-Babu-Super-HD-Stills-from-Maharshi-.jpg?quality=80&zoom=1&ssl=1'}/>
+      
+      <Movie img={'https://i.pinimg.com/originals/8a/f3/61/8af36186ee2e3bf86e466b8fc1ca5cc6.jpg'}
+       name={ 'Sarileru'}  year={'2020 , mahesh'} time={'119 min'} about={' Action, Drama'}
+         rating={'https://o.remove.bg/downloads/26a2c8c8-41d5-46b0-a9b0-d1d92d1354b6/7d4cf105cef06615cd1948b7af70a7d4-removebg-preview.png'}
+         num='4.5'
+         description={'A tough army major is deployed to Kurnool on a mission to keep the country safe from external threats. Director: Anil Ravipudi | Stars: Mahesh Babu, Rashmika Mandanna, Vijayshanti, Rajendra Prasad '}
+         
+         fullimage={'https://wallpaperaccess.com/full/4009615.jpg'}/>
+
+
+
+<Movie img={'https://m.media-amazon.com/images/M/MV5BOWNkMWQ3MDEtYjQ3Zi00Y2NhLWE1NzYtZGJkM2NlYWY3OWFmXkEyXkFqcGdeQXVyNDY5MTUyNjU@._V1_UX140_CR0,0,140,209_AL_.jpg'}
+       name={ 'Pokiri'}  year={'2006 , mahesh'} time={'119 min'} about={' Action, Drama,fire'}
+         rating={'https://o.remove.bg/downloads/26a2c8c8-41d5-46b0-a9b0-d1d92d1354b6/7d4cf105cef06615cd1948b7af70a7d4-removebg-preview.png'}
+         num='4.5'
+         description={'A money-minded criminal has to deal with two rival gangs as well as an evil cop threatening his girlfriend. '}
+         
+         fullimage={'https://m.media-amazon.com/images/M/MV5BOWNkMWQ3MDEtYjQ3Zi00Y2NhLWE1NzYtZGJkM2NlYWY3OWFmXkEyXkFqcGdeQXVyNDY5MTUyNjU@._V1_UX140_CR0,0,140,209_AL_.jpg'}/>
+
+
+
+      
+       </div>
+       
+
+
+
+
+
+         
+      
+   
   );
 }
 
---
+++ src/components/Movie.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import './Movie.css'
+
+
+function Movie(props)
+{
+    return(
+         <div className='main'>
+        <div className='card'>
+        
+       
+            <img  id='mabu' src={props.img} alt='sarkar'/>
+            <h4> {props.name} </h4>
+            <p>{props.year}</p>
+            <button>{props.time}</button>
+            <p id='m'>{props.about}</p>
+            <img id='rat' src={props.rating} alt='rate'/>
+            <p id='num'>{props.num}</p>
+             <div className='mab'>
+  
+             <p id='des'>{props.description}</p>
+   
+             </div>
+          <img id='full' src={props.fullimage} alt='full'/>
+ 
+  
+            
+            
+       
+       
+        </div>
+         </div>
+    )
+}
+export default Movie
